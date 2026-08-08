c2af79d293a80816b55acc68a7df06c968edd886
FusionZhu <tianlangstudio@aliyun.com>
2026-08-08 17:31:54 +0800

    Upgraded dependencies version and added features

--- rboot/src/lib.rs
+++ rboot/src/lib.rs
@@ -1,7 +1,10 @@
-
+#[cfg(feature = "db")]
 pub mod database;
 
 pub use tl_log as log;
 pub use tl_config as config;
+#[cfg(feature = "task")]
 pub use tl_task as task_scheduler;
+#[cfg(feature = "jwt")]
 pub use tl_jwt as jwt;
+pub use anyhow as anyhow;
